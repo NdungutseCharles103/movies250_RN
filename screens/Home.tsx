088b1502afbf7782b079bc9aa79904459a5af6c1
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-09-22 20:52:52 +0200

    changed some app behaviour

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,4 +1,10 @@
-import { Dimensions, FlatList, Image, ScrollView, StyleSheet } from "react-native";
+import {
+	Dimensions,
+	FlatList,
+	Image,
+	ScrollView,
+	StyleSheet,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
@@ -9,50 +15,47 @@ import tw from "twrnc";
 import useTheme from "../hooks/useTheme";
 import { height, width } from "../constants/Layout";
 import MovieCard from "../components/MovieCard";
+import Video from 'react-native-video'
 
-const ncols = ()=> {
-	if(width>700) return 4
-	else if(width>500) return 3
-	else if(width>325) return 2
-	else return 1
-}
+const ncols = () => {
+	if (width > 700) return 4;
+	else if (width > 500) return 3;
+	else if (width > 325) return 2;
+	else return 1;
+};
 
 export default function Home({ navigation }: RootTabScreenProps<"Home">) {
-	const [cols, setCols] = useState(ncols())
+	const [cols, setCols] = useState(ncols());
 	const { data, isLoading, setPath } = useFetch("now_playing");
 	const theme = useTheme();
 	const [active, setActive] = useState<string>("now_playing");
-	// Dimensions.addEventListener('change', ()=> {
-	// 	const width = Dimensions.get("window").width
-	// 	if(width>700) setCols(4)
-	// 	else if(width>500) setCols(3)
-	// 	else if(width>325) setCols(2)
-	// 	else setCols(1)
-	// })
 
 	useEffect(() => {
 		console.log(data);
 	}, [data]);
 
-  const renderItem = ({ item }: any) => {
-    return (
-      <MovieCard
-        item={item}
-        size={cols}
-      />
-    );
-  };
+	const renderItem = ({ item }: any) => {
+		return <MovieCard item={item} size={cols} />;
+	};
 
 	return (
 		<View style={styles.container}>
-			<TopSlider tabs={homeTabs} active={active} setPath={setPath} setActive={setActive} />
+			<TopSlider
+				tabs={homeTabs}
+				active={active}
+				setPath={setPath}
+				setActive={setActive}
+			/>
 			<ScrollView style={[tw`h-full w-[100%]`]}>
 				<FlatList style={tw``} data={data?.results} keyExtractor={(item: any) => item.alt}
          			renderItem={renderItem} numColumns={cols} />
-					{/* {movies?.results?.map((movie: any) => (
-						<MovieCard movie={movie} />
-					))} */}
 			</ScrollView>
+			{/* <Video
+				source={{
+					uri: "https://assets.mixkit.co/videos/download/mixkit-countryside-meadow-4075.mp4",
+				}}
+				volume={10}
+			/> */}
 		</View>
 	);
 }
@@ -64,4 +67,4 @@ const styles = StyleSheet.create({
 		width: width,
 		height: height,
 	},
-})
+});
